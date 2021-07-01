036e6c9630caf80bd02fa4439b0335174f434f1d
Sergiy Mashtaler <sergiy.mashtaler@digib.com>
2021-07-01 17:12:12 +0200

    cicd-acc-id

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from aws_cdk import core
 
 from pipelines.pipeline_stack import PipelineStack
 
-PIPELINE_ACCOUNT = '997796868421'
+PIPELINE_ACCOUNT = '045670552235'
 
 app = core.App()
 PipelineStack(app, 'PipelineStack', env={
